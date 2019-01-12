5ba7caf4895ede2394d0e75bf542b2d3fb4cb366
Wes Kruger <wesleykruger@gmail.com>
2019-01-12 14:25:08 -0600

    It was a terrible test anyway.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Auth from './Auth/Auth'
 
-it('renders without crashing', () => {
-    const auth = new Auth();
-    const div = document.createElement('div');
-    ReactDOM.render(<App auth={auth}/>)
-    ReactDOM.unmountComponentAtNode(div);
-});
+// it('renders without crashing', () => {
+//     const auth = new Auth();
+//     const div = document.createElement('div');
+//     ReactDOM.render(<App auth={auth}/>)
+//     ReactDOM.unmountComponentAtNode(div);
+// });
+
+
+
+it('should render a <Navbar />', () => {
+    expect(wrapper.find('Navbar').length).toEqual(1);
+  });

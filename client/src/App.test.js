1bdce281bb5da93944ee74bcf4f7d808aa600ed6
Wes Kruger <wesleykruger@gmail.com>
2019-01-12 14:22:20 -0600

    props don't exist?

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -6,6 +6,6 @@ import Auth from './Auth/Auth'
 it('renders without crashing', () => {
     const auth = new Auth();
     const div = document.createElement('div');
-    ReactDOM.render(<App auth={auth} {...props} />)
+    ReactDOM.render(<App auth={auth}/>)
     ReactDOM.unmountComponentAtNode(div);
 });

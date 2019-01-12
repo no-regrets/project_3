a046623e39ad3ac0df514b774e6d7ebcc6e1a885
Wes Kruger <wesleykruger@gmail.com>
2019-01-12 14:19:34 -0600

    Updated App test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Auth from './Auth'
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it('renders without crashing', () => {
+    const auth = new Auth();
+    const div = document.createElement('div');
+    ReactDOM.render(<App auth={auth} {...props} />)
+    ReactDOM.unmountComponentAtNode(div);
+});

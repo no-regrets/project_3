4575ff09100fb73e6cac7e77a7ef513312d79bb6
Casey <casey.j.lee@gmail.com>
2019-01-16 19:57:42 -0600

    check for taintedness

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 // import { makeMainRoutes } from './routes';
-
+//test change
 
 // const routes = makeMainRoutes();
 

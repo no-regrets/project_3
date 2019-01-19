a1b86296bd708136cfe20fc0405cc05b08d4990f
Wes Kruger <wesleykruger@gmail.com>
2019-01-19 13:01:45 -0600

    testing hardcoded callback uri

--- client/src/Auth/Auth.js
+++ client/src/Auth/Auth.js
@@ -13,7 +13,7 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: AUTH_CONFIG.domain,
     clientID: AUTH_CONFIG.clientId,
-    redirectUri: AUTH_CONFIG.callbackUrl,
+    redirectUri: "https://noregrets-project3.herokuapp.com/callback",
     responseType: 'token id_token',
     scope: 'openid profile'
   });

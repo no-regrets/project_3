62843084dd8bfab874f3db70cfd26611e729bec7
Wes Kruger <wesleykruger@gmail.com>
2019-01-23 21:32:29 -0600

    correct auth Config'

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'wkruger.auth0.com',
   clientId: 'pPl4zpuRsL0XqcVCtTi1FZcX4yt7O4HF',
-  callbackUrl: 'http://localhost:3000/callback'
-  // 'https://noregrets-project3.herokuapp.com/callback'
+  // callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://noregrets-project3.herokuapp.com/callback'
 }

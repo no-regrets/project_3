2432c3bee76c201bfe799d20032f34ee21b8dd08
Joseaph Mankin <JoseaphMankin@gmail.com>
2019-01-26 12:30:22 -0600

    Tucking Auth0 in an .env.
    Hardcoded now in Heroku

--- client/src/components/Authorization/auth0-variables.js
+++ client/src/components/Authorization/auth0-variables.js
@@ -1,6 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: "wkruger.auth0.com",
-  clientId: "pPl4zpuRsL0XqcVCtTi1FZcX4yt7O4HF",
-  // callbackUrl: "http://localhost:3000/callback"
-  callbackUrl: "http://localhost:3000/callback",//"https://noregrets-project3.herokuapp.com/callback"
+  domain: process.env.REACT_APP_DOMAIN,
+  clientId: process.env.REACT_APP_CLIENTID,
+  callbackUrl: process.env.REACT_APP_CALLBACKURL
 };

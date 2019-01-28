075ed38b5b3ff91dffb28688dd9502662ae41dea
Wes Kruger <wesleykruger@gmail.com>
2019-01-27 23:08:02 -0600

    How in the world did server logic get updated?

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/noRegretsDB" //the correct db
-);
 // // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/noRegretsDB", { useNewUrlParser: true }

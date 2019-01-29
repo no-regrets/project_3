5f2f7ea163de33feeb728142e8e5bc1eebb7d7f9
Joseaph Mankin <JoseaphMankin@gmail.com>
2019-01-28 19:32:31 -0600

    Updated the createdAt and endedAt into Moment.js dates

--- models/session.js
+++ models/session.js
@@ -5,8 +5,8 @@ const sessionSchema = new Schema({
   drinkGoal: { type: Number, required: false, default: 3 },
   maxBAC: { type: Number, required: false, default: 3 },
   budget: {type: Number, required: false, default: 50 },
-  createdAt: { type: Date, default: Date.now },
-  endedAt: { type: Date, required: false },
+  createdAt: { type: String, required: false },
+  endedAt: { type: String, required: false },
   inProgress: {type: Boolean, required: false, default: true},
   drink: [
     {
